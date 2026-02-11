dd366e83ac0bed0c6d19919cce40b58811226b57
Debi prasad Das <dddebiprasaddas2004@gmail.com>
2026-02-11 10:10:44 +0530

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://cloudnest-backend-ie4m.onrender.com';
 
 // Exporting individual service prefixes for flexibility
 export const AUTH_BASE = `${API_BASE_URL}/api/auth`;
